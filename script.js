5377ce018b8eca25053ad3dd175deb31cef683a5
Your Name <daldrin287@gmail.com>
2024-03-31 21:12:14 +0800

    i change animation and add content to letter

---
+++ script.js
@@ -0,0 +1,30 @@
+const envelope = document.querySelector('.envelope');
+const openBtn = document.querySelector('.open-btn');
+
+openBtn.addEventListener('click', () => {
+envelope.classList.toggle('open');
+});
+
+
+//verification of name
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  if (password.toLowerCase() === "claudia") {
+    document.getElementById("password-form").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+  } else {
+    alert("who r u?");
+  }
+}
+
+
+const mainContent = document.getElementById('main-content');
+const letterItself = document.getElementById('letterItself');
+
+mainContent.addEventListener('click', function() {
+  mainContent.classList.add('open');
+  setTimeout(function() {
+    letterItself.style.display = 'block';
+    mainContent.style.display = 'none';
+  }, 2000);
+});
